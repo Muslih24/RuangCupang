a8bcb5497af788b8ff60d4cf7a82db33e23880ff
Muslih24 <Muslih259@gmail.com>
2021-02-12 13:06:29 +0700

    Update Jenis_add.php

--- application/views/Backend/Jenis/Jenis_add.php
+++ application/views/Backend/Jenis/Jenis_add.php
@@ -6,18 +6,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tambah Jenis</title>
 </head>
-<form action="<?php echo base_url('Backend/Jenis/ProsesAddJenis'); ?>" method="POST">
-    <div class="form-group">
-        <label for="jenis">Jenis</label>
-        <input type="hidden" name="id_jenis">
-        <input type="text" name="jenis" id="jenis">
-        <small class="form-text text-danger"><?= form_error('jenis'); ?></small>
-    </div>
-    <button type="submit">Tambah Jenis</button>
-</form>
-
 
 <body>
+    <form action="<?php echo base_url('Backend/Jenis/ProsesAddJenis'); ?>" method="POST">
+        <div class="form-group">
+            <label for="jenis">Jenis</label>
+            <input type="hidden" name="id_jenis">
+            <input type="text" name="jenis" id="jenis">
+            <small class="form-text text-danger"><?= form_error('jenis'); ?></small>
+        </div>
+        <button type="submit">Tambah Jenis</button>
+    </form>
+
 </body>
 
 </html>
